4986b96e5bb90e7fd183c4ff9790c337149bbd50
Sangeun <smddkt@yonsei.ac.kr>
2024-04-24 11:23:17 +0900

    add daynight button

---
+++ daynight_button.js
@@ -0,0 +1,37 @@
+var links = {
+    setColor: function(color){
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i<alist.length){
+    //     alist[i].style.color = color;
+    //     i+= 1;}
+    // }
+    $('a').css('color', color);
+    }
+}
+var Body = {
+    setColor:function (color){
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color)
+    },
+    setBackgroundColor: function (color){
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color)
+        }
+    }
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value==='night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value='day';
+    
+        links.setColor('powderblue')
+    }else{
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value='night';
+    
+        links.setColor('blue')
+        }
+    }
